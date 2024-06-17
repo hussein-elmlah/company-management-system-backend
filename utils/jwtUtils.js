de30ec5e0aced7292b5f35bc443fcf7c05fef095
hussein-elmlah <husseinelmlah11@gmail.com>
2024-06-18 02:53:38 +0300

    Add jwtUtils.js with generateTokenUser function for JWT token generation

---
+++ utils/jwtUtils.js
@@ -0,0 +1,25 @@
+import jsonwebtoken from 'jsonwebtoken';
+
+const { JWT_SECRET } = process.env;
+
+export const generateTokenUser = (user) => {
+  try {
+    if (!user || !user.username || !user._id || !user.role) {
+      return new Error('Invalid user object.');
+    }
+
+    if (!JWT_SECRET) {
+      return new Error('JWT secret is not defined.');
+    }
+
+    const token = jsonwebtoken.sign(
+      { username: user.username, id: user._id, role: user.role },
+      JWT_SECRET,
+      { expiresIn: '7d' },
+    );
+    return token;
+  } catch (error) {
+    console.error('Error generating JWT token:', error.message);
+    throw error;
+  }
+};
