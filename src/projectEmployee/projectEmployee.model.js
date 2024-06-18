65ab7b0f7e56377b5436a62f733cad4548ccd90b
hussein-elmlah <husseinelmlah11@gmail.com>
2024-06-18 04:01:25 +0300

    Add ProjectEmployee model

---
+++ src/projectEmployee/projectEmployee.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const projectEmployeeSchema = new mongoose.Schema({
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  hoursWorked: {
+    type: Number,
+    default: 0
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'on leave'],
+    default: 'active'
+  },
+},
+{ 
+  timestamps: true,
+  runValidators: true,
+},
+);
+
+// Define compound unique index on project and employee fields
+projectEmployeeSchema.index({ project: 1, employee: 1 }, { unique: true });
+
+const ProjectEmployee = mongoose.model('ProjectEmployee', projectEmployeeSchema);
+
+export default ProjectEmployee;
