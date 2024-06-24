2e7e5ad8cdbb07d8659f5225b9b5f843dd4e8698
esraamagddi <esraamagdy20133@gmail.com>
2024-06-24 20:40:48 +0300

    feat:add subject to mail

--- src/modules/contact/contact.controllers.js
+++ src/modules/contact/contact.controllers.js
@@ -4,19 +4,18 @@ import CustomError from '../../../lib/customError.js';
 import sendEmail from '../../../utils/sendEmail.js';
 
 export const createContact = asyncHandler(async (req, res) => {
-  // console.log(req.body)
-  const { name, email, message } = req.body;
+  const { name, email, message, subject } = req.body;
 
-  if (!name || !email || !message) {
+  if (!name || !email || !message || !subject) {
     throw new CustomError('All fields are required', 400);
   }
 
+
   const contact = new Contact({ name, email, message });
   await contact.save();
-
-  const emailSubject = `New Contact Us Message from ${name}`;
+  const emailSubject = subject;
   const emailText = `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`;
-  await sendEmail('recipient-email@example.com', emailSubject, emailText);
+  await sendEmail(process.env.EMAIL_USER, emailSubject, emailText);
 
   res.status(201).json({ message: 'Message sent successfully' });
 });
