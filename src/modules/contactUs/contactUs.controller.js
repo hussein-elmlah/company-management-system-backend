0e710aa107f53dc6d52e33f7b55d904b75761657
esraamagddi <esraamagdy20133@gmail.com>
2024-06-24 20:22:49 +0300

    feat:handling contactus fanctionality

--- src/modules/contactUs/contactUs.controller.js
+++
@@ -1,4 +0,0 @@
-export const createContact = asyncHandler(async (req, res) => {
-  console.log("createContact");
-  });
-  
--- src/modules/contactUs/contactUs.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as contactController from '../controllers/contact.controllers.js';
-
-const router = express.Router();
-
-router.post('/contactus', contactController.createContact);
-
-export default router;
